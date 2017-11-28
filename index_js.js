b82e9fa8a1afd952d958b08368515dad93f8731d
Ariel-Zhou <Ariel-Zhou@users.noreply.github.com>
2017-11-28 22:47:26 +0800

    c

--- index_js.js
+++ index_js.js
@@ -9,3 +9,15 @@ window.onload = function () {
         b.style.display = "none";
     }
 }
+
+
+window.onload = function () {
+    var c = document.getElementById("11");
+    var d = document.getElementById("22");
+    c.onmouseover = function () {
+        d.style.display = "block";
+    }
+    c.onmouseout = function () {
+        d.style.display = "none";
+    }
+}
